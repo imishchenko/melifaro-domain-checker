503a448c60571bf0bb829747e332451d7d440b09
Iakov Mishchenko <iakov.mishchenko@mrcplast.com>
2013-03-10 14:05:51 +0200

    Initial commit

---
+++ DomainZone/UnsupportedDomainZoneException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Melifaro\DomainChecker\DomainZone;
+
+/**
+ * Unsupported Domain Zone Exception
+ * 
+ * @package MelifaroDomainChecker
+ * @author Iakov Mishchenko <mail@iakov.net>
+ */
+class UnsupportedDomainZoneException extends \Exception
+{
+    
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Melifaro Domain Checker
+=======================
+Author: Iakov Mishchenko <mail@iakov.net>
+Version: 0.1
+
